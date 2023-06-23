30c09600fc5a046beb946b288e932fe48dce86d6
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-06-24 02:20:38 +0800

    Add script for modalUX features

--- src/apps/modalUX.js
+++ src/apps/modalUX.js
@@ -1,30 +1,92 @@
 const modalUX = (function () {
 
+    // Closes/ unselect all buttons from given nodeList
+    const closeBtns = function (nodeList) {
+        nodeList.forEach(btn => {
+            if (btn.dataset.selected === 'selected') {
+                btn.dataset.selected = "";
+            }
+        });
+    }
+
+    // Set the default selected button on modal closing
+    // Note: includes closeBtns function, 1st argument is nodeList
+    const setDefaultBtn = function (nodeList, defaultBtn) {
+        // Ensures other buttons are closed
+        closeBtns(nodeList);
+
+        // Defaults newEventBtn as selected
+        defaultBtn.dataset.selected = 'selected';
+    }
+
+    // "Add" Options buttons UX
+    const addOptionsBtns = function (action) {
+        const optionsBtns = document.querySelectorAll('div#add-options button');
+        const newEventBtn = document.querySelector('button#new-event');
+
+        const markBtn = function () {
+            const selectedBtn = this;
+
+            // Ensures other buttons are closed
+            closeBtns(optionsBtns);
+
+            // Change UI for selected Btn
+            selectedBtn.dataset.selected = 'selected';
+        }
+
+        if (action === true) {
+            optionsBtns.forEach(btn => btn.addEventListener('click', markBtn));
+
+        } else if (action === false) {
+            setDefaultBtn(optionsBtns, newEventBtn); // Sets newEventBtn on modal closing
+            optionsBtns.forEach(btn => btn.removeEventListener('click', markBtn));
+        }
+    }
+
     // UX for select event priority
-    const addPriorityButtonEvent = function () {
+    const addPriorityBtnEvent = function (action) {
         const prioBtns = document.querySelectorAll('button.prio-btn');
+        const midPrioBtn = document.querySelector('button[data-id="mid"]');
 
-        const markButton = function () {
-            console.log(this);
-
+        const markBtn = function () {
             const prioButnId = this.dataset.id;
             const thisPrioBtn = document.querySelector(`button[data-id='${prioButnId}']`);
         
             // Ensures only one container is marked
-            prioBtns.forEach(btn => {
-                if (btn.hasAttribute('data-selected')) {
-                    btn.removeAttribute('data-selected');
-                }
-            });
+            closeBtns(prioBtns);
         
             // Marks container with checked radio
             thisPrioBtn.setAttribute('data-selected', 'selected');
         }
-        
-        prioBtns.forEach(radio => radio.addEventListener('click', markButton));
+
+
+        // Conditional for adding and removing eventListeners
+        if (action === true) {
+            prioBtns.forEach(btn => btn.addEventListener('click', markBtn));
+
+        } else if (action === false) {
+            setDefaultBtn(prioBtns, midPrioBtn); // Sets midPrioBtn on modal closing
+            prioBtns.forEach(btn => btn.removeEventListener('click', markBtn));
+        }
     }
 
-    return { addPriorityButtonEvent }
+    // Compiled function to add all necessary listeners and UX
+    const addModalUX = function () {
+
+        // Reminder: true argument to add eventListeners, false means remove
+        addPriorityBtnEvent(true);
+        addOptionsBtns(true);
+    }
+
+    // Compiled function to remove all listeners and UX
+    const removeModalUX = function () {
+
+        // Reminder: false to remove eventListeners
+        addPriorityBtnEvent(false);
+        addOptionsBtns(false);
+    }
+
+    return { addModalUX, removeModalUX }
 })();
 
 export { modalUX };
--- src/apps/showModals.js
+++ src/apps/showModals.js
@@ -2,21 +2,38 @@ import { modalUX } from "./modalUX";
 
 const showModals = (function () {
 
+    // Opens Modal and adds required eventListeners to elements inside the modal
     const showAddOptions = function () {
         const addPromptModal = document.querySelector('dialog#add-prompt');
+        const closeBtn = document.querySelector('button#close-add');
+
         addPromptModal.showModal();
 
+        // Add eventListener to close-add/ close modal button
+        closeBtn.addEventListener('click', closeModal);
+
         // Add additional event listeners upon show modal
-        modalUX.addPriorityButtonEvent();
+        modalUX.addModalUX();
     } 
 
+    // Closes Modal and removes eventListeners to elements inside the modal
+    const closeModal = function () {
+        // Removes event listeners from elements inside modal
+        modalUX.removeModalUX();
+
+        // Closes modal
+        const addPromptModal = document.querySelector('dialog#add-prompt');
+        addPromptModal.close();
+    }
+
+    // Adds event listener to addButton
     const addButtonEvent = function () {
         const addButton = document.querySelector('button#add');
         addButton.addEventListener('click', showAddOptions);
     }
 
     
-    return { showAddOptions, addButtonEvent }
+    return { addButtonEvent }
 })();
 
 export { showModals };
