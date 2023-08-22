f3ffb817b3992daff181c63ebcfe9ee0aa443626
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-08-23 01:26:38 +0800

    Adds script for adding events to project
    Initialize UI for adding/ linking events to project in the new project form

--- src/apps/addEventToProject.js
+++ src/apps/addEventToProject.js
@@ -2,22 +2,78 @@ import { memoryHandler } from "./memoryHandler";
 
 
 const addEventToProject = (function () {
+    let newProjectEvents = {};
+
+    const resetProjectEvents = function () {
+        newProjectEvents = {};
+    }
+
+    const getProjectEvents = function () {
+        return newProjectEvents;
+    }
+
+    const getEventsCount = function () {
+        const eventsCount = Object.keys(getProjectEvents()).length;
+        return eventsCount;
+    }
+
+    const createEventObj = function () {
+    }
 
     const addEventsToDOM = function (obj) {
         const eventObj = obj;
 
-        console.log(eventObj);
+        const eventsCont = document.querySelector('div#events-container');
+
+        // Create new event container div
+        const newEventCont = document.createElement('div');
+        newEventCont.setAttribute('data-id', 'event-1');
+        newEventCont.setAttribute('class', 'event-link-project');
+
+        // Create icon
+        const eventIcon = document.createElement('div');
+        eventIcon.setAttribute('class', 'event-icon');
+
+        // Create event title
+        const eventTitle = document.createElement('p');
+        eventTitle.setAttribute('class','project-event');
+        eventTitle.textContent = eventObj.title;
+        
+        //Create unlink button
+        const unlinkButton = document.createElement('button');
+        unlinkButton.setAttribute('type', 'button');
+        unlinkButton.setAttribute('class', 'unlink-event');
+        unlinkButton.dataset.id = eventObj.eventId;
+
+        //Append components to newEventCont
+        const components = [eventIcon, eventTitle, unlinkButton];
+        components.forEach(comp => newEventCont.appendChild(comp));
+
+        //Append newEventCont to eventsCont
+        eventsCont.appendChild(newEventCont);
     }
 
     const addEvent = function () {
         const eventSelect = document.querySelector('select#add-event-select');
-
+        
         // Check if value was selected or not
         if (eventSelect.value === '') {
             return;
         }
 
+        const eventOption = eventSelect.querySelector(`option[value="${eventSelect.value}"]`);
+        const defaultOption = eventSelect.querySelector('option[value=""]');
+
         const eventObj = memoryHandler.getEvents().find((event) => event.eventId === eventSelect.value );
+
+        // Create an event identifier object
+        newProjectEvents[`event-${getEventsCount() + 1}`] = eventSelect.value;
+        console.log(getProjectEvents());
+
+        // Disables the selected event from choices, the turn the select to default value
+        defaultOption.selected = true;
+        eventOption.disabled = true;
+
 
         addEventsToDOM(eventObj);
     }
--- src/apps/eventDelete.js
+++ src/apps/eventDelete.js
@@ -1,16 +1,8 @@
-import { memoryHandler } from "./memoryHandler";
-import { eventsDisplay } from "./eventsDisplay";
-import { onLoadScreen } from "./onLoadScreen";
 import { showModals } from "./showModals";
 
 const eventDelete = (function () {
     const showDeletePrompt = function () {
         const eventId = this.dataset.id;
-
-        // memoryHandler.deleteEvent(eventId);
-        // onLoadScreen.displayEventsCount();
-
-        // eventsDisplay.displayEventsToDOM();
 
         // Show/ Display confirm delete prompt modal
         showModals.showEventDeletePrompt(eventId);
